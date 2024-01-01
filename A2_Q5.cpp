2598741c0a2d24fabb15c79c0965f57a38fec454
Riya822 <144415293+Riya822@users.noreply.github.com>
2024-01-01 21:10:45 +0530

    Create A2_Q5.cpp

---
+++ A2_Q5.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int n,i,j;
+    cout<<"enter a number :";
+    cin>>n;
+    for(i=0;i<n;i++){
+        for(j=n;j>i;j--)
+        cout<<" ";
+        for(j=0;j<i;j++)
+        cout<<"*"<<" ";
+        cout<<endl;
+    }
+    for(i=n;i>0;i--){
+        for(j=n;j>i;j--)
+        cout<<" ";
+        for(j=0;j<i;j++)
+        cout<<"*"<<" ";
+        cout<<endl;
+    }
+}
