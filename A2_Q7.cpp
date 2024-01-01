1e22a371964881eafa5a5845027ce85852699397
Riya822 <144415293+Riya822@users.noreply.github.com>
2024-01-01 21:42:05 +0530

    Create A2_Q7.cpp

---
+++ A2_Q7.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int i,j,n;
+    cout<<"enter a number :";
+    cin>>n;
+    for(i=0;i<n;i++){
+        for(j=0;i>j;j++)
+        cout<<"  ";
+        cout<<" *";
+        cout<<endl;
+    }
+}
