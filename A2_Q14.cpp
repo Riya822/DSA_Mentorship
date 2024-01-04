d249b7cda66c82fd592e8241e961cdc6dd45be58
Riya822 <144415293+Riya822@users.noreply.github.com>
2024-01-05 01:56:33 +0530

    Create A2_Q14.cpp

---
+++ A2_Q14.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std; 
+int main(){
+    int n,i;
+    cout<<"enter a number :";
+    cin>>n;
+    for(i=1;i<=n;i++){
+        cout<<"x*"<<i<<"="<<i<<"x";
+        cout<<endl;
+    }
+}
